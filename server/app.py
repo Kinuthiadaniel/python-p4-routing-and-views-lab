b9f7981d33e4481f4e180741affc97ffacd989d8
Kinuthiadaniel <dankinuthia17@gmail.com>
2024-06-25 17:26:05 +0300

    commit

--- server/app.py
+++ server/app.py
@@ -1,8 +1,49 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, abort
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)
+    return parameter
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    count = f''
+    for i in range(parameter):
+        count += f'{i}\n'
+    return count
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        return str(num1 + num2)
+    elif operation == '-':
+        if num1 < num2:
+            return str(num2 - num1)
+        return str(num1 - num2)
+    elif operation == '*':
+        return str(num1 * num2)
+    elif operation == 'div':
+        if num2!= 0:
+            return str(num1 / num2)
+        else:
+            return 'Cannot divide by 0'
+    
+    elif operation == '%':
+        return str(num1 % num2)
+    else:
+        abort(404)
+
+
+
+    
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
